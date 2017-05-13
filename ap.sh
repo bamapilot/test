e80ba612c4471664b96a2c9da10e3364ed0a3c63
bamapilot <lchspilot@gmail.com>
2017-05-12 19:28:17 -0500

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -3,12 +3,12 @@ sudo sed -i -e "s/\(ssid=\).*/\1$1/" /etc/hostapd/hostapd.conf
 sudo sed -i -e "s/\(passphrase=\).*/\1$2/" /etc/hostapd/hostapd.conf
 sudo /etc/init.d/hostapd stop
 sudo /etc/init.d/udhcpd stop
-sudo ifdown wlan0
-sudo ifconfig wlan0 down
+sudo ifdown wlan1
+sudo ifconfig wlan1 down
 sudo rm -rf /etc/network/interfaces
 sudo cp /etc/network/interfaces.ap /etc/network/interfaces
-sudo ifconfig wlan0 up
-sudo ifup wlan0
+sudo ifconfig wlan1 up
+sudo ifup wlan1
 sudo /etc/init.d/hostapd restart
 sudo /etc/init.d/udhcpd restart
 sudo service hostapd status
